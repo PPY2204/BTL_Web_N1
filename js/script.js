8d5be8e0fe40493ee58c3408677640e3b87cd233
KaizetI <vnpocketmine@gmail.com>
2024-03-25 21:32:49 +0700

    ...

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+$(window).scroll(scrollFunction);
+
+function scrollFunction() {
+if ($(window).scrollTop() > 120) {
+    $(".header").addClass("activeHeader");
+    $(".header__logo").hide();
+    $(".header__iconBox").hide();
+} else {
+    $(".header").removeClass("activeHeader");
+    $(".header__logo").show();
+    $(".header__iconBox").show();
+}
+}
 $(document).ready(function() {
 //   alert('Hello, World!');
   let selectedTab = "home";
